80128423b838e89f48376954ec0249df75e7d902
Rahul <rahul.s@sparshmail.com>
2021-02-08 13:07:33 +0530

    Added Separate header and params functions

--- retrofitnetworkcall/src/main/java/com/retrofit/sparsh/retrofitnetworkcall/RetrofitNetworkCall.kt
+++ retrofitnetworkcall/src/main/java/com/retrofit/sparsh/retrofitnetworkcall/RetrofitNetworkCall.kt
@@ -3,6 +3,7 @@ package com.retrofit.sparsh.retrofitnetworkcall
 import androidx.lifecycle.MutableLiveData
 import com.lottery.quico.utils.ApiClient
 import com.lottery.quico.utils.ApiInterface
+import retrofit2.Response
 import java.util.*
 
 /**
@@ -16,15 +17,49 @@ class RetrofitNetworkCall {
         suspend fun initNetworkCall(
             baseUrl: String,
             endpoint: String,
-            headers: HashMap<String, String>,
-            params: HashMap<String, String>,
-            observable: MutableLiveData<String>
+            headers: HashMap<String, String>?,
+            params: HashMap<String, String>?,
+            observable: MutableLiveData<String>,
+            type: String
         ) {
             val apiService: ApiInterface =
                 ApiClient.getClient(baseUrl).create(ApiInterface::class.java)
+            var response: Response<String>? = null
 
-            val response = apiService.getRequest(endpoint, headers, params)
-            if (response.isSuccessful) {
+            if (type.equals("get", ignoreCase = true)) {
+
+                if ((headers != null) && (params != null)) {
+                    response =
+                        apiService.getWithHeaderRequest(endpoint + "/" + params, headers)
+                }
+                if ((headers != null) && (params == null)) {
+                    response = apiService.getWithHeaderRequest(endpoint, headers)
+                }
+                if ((headers == null) && (params != null)) {
+                    response = apiService.getRequest(endpoint + "/" + params)
+                }
+                if ((headers == null) && (params == null)) {
+                    response = apiService.getRequest(endpoint)
+                }
+            }
+            if (type.equals("post", ignoreCase = true)) {
+
+                if ((headers != null) && (params != null)) {
+                    response = apiService.postWithHeaderAndParamsRequest(endpoint, headers, params)
+                }
+                if ((headers != null) && (params == null)) {
+                    response = apiService.postWithHeaderRequest(endpoint, headers)
+                }
+                if ((headers == null) && (params != null)) {
+                    response = apiService.postWithParamsRequest(endpoint, params)
+                }
+                if ((headers == null) && (params == null)) {
+                    response = apiService.postRequest(endpoint)
+                }
+            }
+
+
+            if (response!!.isSuccessful) {
                 observable!!.postValue(response.body())
             } else {
                 observable!!.postValue(errorCode)
--- retrofitnetworkcall/src/main/java/com/retrofit/sparsh/retrofitnetworkcall/util/ApiInterface.kt
+++ retrofitnetworkcall/src/main/java/com/retrofit/sparsh/retrofitnetworkcall/util/ApiInterface.kt
@@ -4,18 +4,47 @@ import retrofit2.http.*
 
 interface ApiInterface {
 
+    /*GET requests starts*/
+
     @GET
     suspend fun getRequest(
         @Url url: String,
-        @HeaderMap headers: Map<String, String>,
-        @Body params: Map<String, String>
     ): retrofit2.Response<String>
+
+    @GET
+    suspend fun getWithHeaderRequest(
+        @Url url: String,
+        @HeaderMap headers: Map<String, String>?,
+    ): retrofit2.Response<String>
+
+    /*GET requests ends*/
+
+    /*POST requests starts*/
 
     @POST
     suspend fun postRequest(
         @Url url: String,
-        @HeaderMap headers: Map<String, String>,
-        @Body params: Map<String, String>
     ): retrofit2.Response<String>
+
+    @POST
+    suspend fun postWithHeaderRequest(
+        @Url url: String,
+        @HeaderMap headers: Map<String, String>?
+    ): retrofit2.Response<String>
+
+    @POST
+    suspend fun postWithParamsRequest(
+        @Url url: String,
+        @Body params: Map<String, String>?
+    ): retrofit2.Response<String>
+
+    @POST
+    suspend fun postWithHeaderAndParamsRequest(
+        @Url url: String,
+        @HeaderMap headers: Map<String, String>?,
+        @Body params: Map<String, String>?
+    ): retrofit2.Response<String>
+
+    /*POST requests ends*/
 
 }
